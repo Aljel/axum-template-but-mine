b4503cf00ef2b374af1b4c2cafed0d4ad7ea031f
aragami3070 <smirnov17612@gmail.com>
2026-04-09 22:16:02 +0400

    feat(repository): add UserRepo struct

--- src/repository/users.rs
+++ src/repository/users.rs
@@ -1,8 +1,9 @@
 use macroses::NewTypeDeref;
-use std::ops::Deref;
+use sqlx::{Pool, Postgres};
+use std::{ops::Deref, sync::Arc};
 use uuid::Uuid;
 
-use crate::models::users::User;
+use crate::{models::users::User, schemas::users::RegisterUser};
 
 #[derive(NewTypeDeref)]
 pub struct Limit(pub u32);
@@ -12,7 +13,19 @@ pub struct Offset(pub u32);
 pub trait UserRepository {
     async fn get_by_id(&self, id: &Uuid) -> Option<User>;
     async fn get(&self, offset: &Offset, limit: &Limit) -> Option<User>;
-    async fn create(&self, user: User);
+    async fn create(&self, user: RegisterUser);
     async fn update(&self, user: User);
 }
+
+#[derive(Clone)]
+pub struct UserRepo {
+    db_pool: Arc<Pool<Postgres>>,
+}
+
+impl UserRepo {
+    pub fn new(db_pool: Arc<Pool<Postgres>>) -> Self {
+        Self { db_pool }
+    }
+}
+
 
