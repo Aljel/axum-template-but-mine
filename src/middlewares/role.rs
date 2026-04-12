5e714f4fb2702ac431967872cb79bf5722e611cc
aragami3070 <smirnov17612@gmail.com>
2026-04-12 21:54:45 +0400

    feat(role-middleware): add RoleLayer struct

---
+++ src/middlewares/role.rs
@@ -0,0 +1,13 @@
+use std::task::{Context, Poll};
+
+use axum::response::IntoResponse;
+use http::Request;
+use tower::{Layer, Service};
+
+use crate::{errors::auth::AuthError, models::users::Role, services::auth::tokens::Claims};
+
+#[derive(Clone)]
+pub struct RoleLayer {
+    allowed_roles: Vec<Role>,
+}
+
