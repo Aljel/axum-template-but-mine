fe922e23b95ea72ad306ecf229e29e28bb0ed1cc
aragami3070 <smirnov17612@gmail.com>
2026-04-12 20:29:25 +0400

    feat(auth-error): add TokenError variant

--- src/errors/auth.rs
+++ src/errors/auth.rs
@@ -2,10 +2,15 @@ use axum::response::IntoResponse;
 use http::StatusCode;
 use thiserror::Error;
 
+use crate::errors::tokens;
+
 #[derive(Error, Debug)]
 pub enum AuthError {
     #[error("database error: {0}")]
     Db(#[from] sqlx::Error),
+
+    #[error("Token service error: {0}")]
+    TokenError(#[from] tokens::TokenError),
 
     #[error("User already exists")]
     UserAlreadyExists,
@@ -17,10 +22,14 @@ pub enum AuthError {
 impl IntoResponse for AuthError {
     fn into_response(self) -> axum::response::Response {
         match self {
-            Self::Db(er) => (StatusCode::INTERNAL_SERVER_ERROR, er.to_string()),
-            Self::UserAlreadyExists => (StatusCode::CONFLICT, "User already exists".to_string()),
-            Self::Unauthorized => (StatusCode::UNAUTHORIZED, "User unauthorized".to_string()),
+            Self::Db(er) => (StatusCode::INTERNAL_SERVER_ERROR, er.to_string()).into_response(),
+            Self::TokenError(er) => er.into_response(),
+            Self::UserAlreadyExists => {
+                (StatusCode::CONFLICT, "User already exists".to_string()).into_response()
+            }
+            Self::Unauthorized => {
+                (StatusCode::UNAUTHORIZED, "User unauthorized".to_string()).into_response()
+            }
         }
-        .into_response()
     }
 }
