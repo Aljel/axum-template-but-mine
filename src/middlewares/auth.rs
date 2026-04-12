74edaf0a9e2f51f918b5875156d162b5fc739482
aragami3070 <smirnov17612@gmail.com>
2026-04-12 20:37:03 +0400

    feat(auth-middleware): add validate access token in AuthMiddleware

--- src/middlewares/auth.rs
+++ src/middlewares/auth.rs
@@ -29,3 +29,40 @@ pub struct AuthMiddleware<S> {
     inner: S,
     token_serv: Arc<TokenService<Postgres>>,
 }
+
+impl<S, ReqBody> Service<Request<ReqBody>> for AuthMiddleware<S>
+where
+    S: Service<Request<ReqBody>, Error = AuthError> + Clone + Send + 'static,
+    S::Future: Send + 'static,
+    ReqBody: Send + 'static,
+{
+    type Response = S::Response;
+    type Error = S::Error;
+    type Future = futures::future::BoxFuture<'static, Result<Self::Response, Self::Error>>;
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.inner.poll_ready(cx)
+    }
+
+    fn call(&mut self, mut req: Request<ReqBody>) -> Self::Future {
+        let mut inner = self.inner.clone();
+        let token_serv = self.token_serv.clone();
+        Box::pin(async move {
+            let auth_header = match req
+                .headers()
+                .get(http::header::AUTHORIZATION)
+                .and_then(|header| header.to_str().ok())
+            {
+                Some(aut_h) => aut_h,
+                None => return Err(AuthError::Unauthorized),
+            };
+
+            let claims = token_serv.validate_access_token(auth_header)?;
+
+            // NOTE: Если токен валидный, то извлеченные claims добавили в запрос
+            req.extensions_mut().insert(claims);
+
+            inner.call(req).await
+        })
+    }
+}
+
